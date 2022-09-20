e8c46028c49df9defa75e98b285035f69c7a081d
DispicableLee <lee.robert053@gmail.com>
2022-09-19 22:13:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCusstomerName(){
+    const newName = customerName.toUpperCase()
+    return newName;
+}
+
+function setBestCustomer(){
+    var bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function oveWriteBestCustomer(){
+    var bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Rob";
+function changeLeastFavoriteCustomer(leastFavoriteCustomer){
+    const leastFavoriteCustomer = "not Rob"
+
+}
